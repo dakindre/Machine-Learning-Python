131a7a303a26036fe27c53dde1913a2b2e7fd0ba
Drew Dakin <dakindre4@gmail.com>
2018-03-30 14:45:48 -0400

    pyplot
    Data collection and plotting added

--- problem1_3.py
+++ problem1_3.py
@@ -1,8 +1,51 @@
 import sys
 import argparse
+import csv
+import matplotlib.pyplot as plt
 
 
-def outputGenerate():
-    f = open('output1.csv','w')
-    f.write('w_1', 'w_2', 'b')
-    f.close()
+
+def plot(values):
+    plt.scatter(values.xi, values.xj, c=values.labels)
+    plt.show()
+    
+    
+class inOut:
+    def __init__(self, inStr, outStr, xi=[], xj=[], labels=[]):
+        self.inStr = inStr
+        self.outStr = outStr
+        self.xi = xi
+        self.xj = xj
+        self.labels = labels
+        self.generateInput()
+
+    def generateInput(self):
+        with open(self.inStr) as csvfile:
+            inputCSV = csv.reader(csvfile, delimiter=',')
+            for row in inputCSV:
+                self.xi.append(float(row[0]))
+                self.xj.append(float(row[1]))
+                self.labels.append(float(row[2]))
+
+    def generateOutput(self):
+        return None
+
+
+                 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('i', type=str)
+    parser.add_argument('o', type=str)
+
+    args = parser.parse_args()
+
+    values = inOut(args.i, args.o)
+    #print('xi ', values.xi)
+    #print('xj ', values.xj)
+    #print('labels ', values.labels)
+
+    plot(values)
+    
+
+if __name__ == "__main__":
+	main()
